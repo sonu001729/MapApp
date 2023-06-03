13d6e83ba5a08ddb3bf9c657f306fc8e7b5b53df
sonu001729 <114768362+sonu001729@users.noreply.github.com>
2023-06-03 15:35:09 +0530

    ....

--- src/MyComponents/MapCall.js
+++ src/MyComponents/MapCall.js
@@ -4,14 +4,10 @@ const MapComponent = () => {
   useEffect(() => {
     const mapSDK = window.MapmyIndia;
     if (mapSDK) {
-      // Use the MapmyIndia SDK here
-      // You can access mapSDK.Map, mapSDK.MapView, etc. to interact with the map
       const map = new mapSDK.Map('map', {
-        center: [28.6139, 77.2090],
-        zoom: 12,
+        center: [13.02528, 80.2062336],
+        zoom: 14,
       });
-
-      // Add map-related functionality here
     }
   }, []);
 
